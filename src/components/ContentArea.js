10eba57b90fdb036f60901990ca7d7d34ae03626
RaduAndreiTaica <andrei.taica@takeofflabs.com>
2024-10-29 20:23:09 +0100

    bachelor + job information

--- src/components/ContentArea.js
+++ src/components/ContentArea.js
@@ -40,11 +40,9 @@ const ContentArea = ({ activeTab }) => {
   const getContentLines = () => {
     switch (activeTab) {
       case '/about/education/bachelor':
-        return ['Bachelor\'s Degree Information', 'Details about the Bachelor\'s degree.'];
-      case '/about/education/masters':
-        return ['Master\'s Degree Information', 'Details about the Master\'s degree.'];
+        return ['Bachelors','Bachelor of Science with Honours in Computer Science,', 'Grade: Class I','City University of London', 'September 2019 - June 2022', '', 'Modules: Introduction to Algorithms, Mathematics for', 'Computing, Systems Architecture, Programming in Java,', 'Databases, Operating Systems, Data Structures and', 'Algorithms, Language Processors, Computer Networks', 'Object-Oriented Analysis and Design, Programming in C++', 'Advanced Programming: Concurrency, Functional Programming,', 'Games Technology'  ];
       case '/about/work/job1':
-        return ['Job 1 Information', 'Details about Job 1.'];
+        return ['TakeOff Labs', 'Full Stack Developer', 'November 2022 - Present', '', 'Tech Stack: Ruby on Rails, Node.js, React.js, React-Native,', 'AWS, Stripe, Git, PostgreSQL, Redis, SideKiq, CLI, EAS, Expo'];
       default:
         return ['ABOUT ME', '', 'Radu Taica, 24', 'Cluj-Napoca, Romania', '', 'I am a problem solver who is interested in solving', 'everything tech. My main background and projects are in Web', 'and Mobile Development, which I had the pleasure of working', 'on during my studies and at my work. I like to work in teams', 'and make meaningful contributions with like-minded people.', '', 'When I am not coding, you will find me at the gym most', 'likely (if you have any crazy calves routine please text', 'it to me) or enjoying some FPS games.', '', 'Feel free to drop a message if you want to talk about tech,', 'lifting, or if you want to team up for a game. I am always', 'up for a good conversation!'];
     }
